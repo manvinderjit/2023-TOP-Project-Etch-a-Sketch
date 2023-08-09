a50e8c35a725db367b5cead40307893aa5030d6d
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2023-08-09 02:08:35 -0400

    feat: Add hover effect function
    Add the hover effect changing the color of grid squares on move hover

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ function createDivGrid (gridSize = 256){
     for(let i=1; i<= gridSize; i++){
         const divSquare = document.createElement('div');
         divSquare.setAttribute("id", i);
-        divSquare.classList.add('divSquare');
+        divSquare.classList.add('divSquare', 'divSquareRegular');
         divContainer.appendChild(divSquare);
     }
 }
 
 createDivGrid();
+
+const divSquaresAll = document.querySelectorAll('.divSquare');
+
+divSquaresAll.forEach((square) => {
+    square.addEventListener('mouseover', (e) => {
+        e.target.classList.remove('divSquareRegular');
+        e.target.classList.add('divSquareHovered');
+    })
+});
