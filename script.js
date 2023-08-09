f7828aafa76c9d49a7550007a25c32a4bede4743
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2023-08-09 14:50:46 -0400

    feat: Add dynamic grid generation
    Add a dynamic grid generation feature that allows users to change the grid size between 1 and 100.

--- script.js
+++ script.js
@@ -1,21 +1,63 @@
 const divContainer = document.querySelector('#container');
 
-function createDivGrid (gridSize = 256){
-    for(let i=1; i<= gridSize; i++){
-        const divSquare = document.createElement('div');
-        divSquare.setAttribute("id", i);
-        divSquare.classList.add('divSquare', 'divSquareRegular');
-        divContainer.appendChild(divSquare);
+function removePreviousChildNodes() {
+    if(!divContainer.hasChildNodes) return;
+    let lastChildNode = divContainer.lastElementChild;
+    while(lastChildNode){
+        divContainer.removeChild(lastChildNode);
+        lastChildNode = divContainer.lastElementChild;
     }
 }
 
-createDivGrid();
+function addHoverEffect() {
+    const divSquaresAll = document.querySelectorAll('.divSquare');
 
-const divSquaresAll = document.querySelectorAll('.divSquare');
+    divSquaresAll.forEach((square) => {
+        square.addEventListener('mouseover', (e) => {
+            e.target.classList.remove('divSquareRegular');
+            e.target.classList.add('divSquareHovered');
+        })
+    });
+}
 
-divSquaresAll.forEach((square) => {
-    square.addEventListener('mouseover', (e) => {
-        e.target.classList.remove('divSquareRegular');
-        e.target.classList.add('divSquareHovered');
-    })
+function createDivGrid (gridSize = 16){
+    if(divContainer.hasChildNodes){
+        removePreviousChildNodes();
+    }
+
+    for(let i=1; i<= Math.pow(gridSize, 2); i++){
+        const divSquare = document.createElement('div');
+        divSquare.setAttribute("id", i);
+        divSquare.classList.add('divSquare', 'divSquareRegular');
+        divSquare.style.width = `${100/gridSize}%`;        
+        divContainer.appendChild(divSquare);
+    }
+
+    addHoverEffect();
+}
+
+function getNewGridSize () {
+    const newGridSize = prompt("Enter the number of square per side! Value between 1 and 100");    
+    console.log(newGridSize);
+    if(newGridSize === null){
+        return newGridSize;
+    }
+    while(parseInt(newGridSize) < 1 || parseInt(newGridSize) > 100){
+        getNewGridSize();
+    }
+    return newGridSize;
+}
+
+const buttonCustomizeGrid = document.querySelector('#customize-grid');
+
+buttonCustomizeGrid.addEventListener('click', () => {
+
+    const newGridSize = getNewGridSize();
+    
+    createDivGrid(
+        newGridSize !== null ? newGridSize : 16
+    );
 });
+
+
+createDivGrid();
